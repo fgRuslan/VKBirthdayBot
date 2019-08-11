b86419635d21d3c7b25760843181d508ca558ff5
Ruslan Isaev <ubijca16@gmail.com>
2019-08-11 11:29:21 +0300

    Move to python3

--- bot.py
+++ bot.py
@@ -1,22 +1,22 @@
 ﻿#!/usr/bin/python
 # -*- coding: utf-8 -*- 
 # Author: https://vk.com/id181265169
-import vk, urllib2, json, random, time, datetime
+import vk, urllib.request, urllib.error, urllib.parse, json, random, time, datetime
 
 config = {}# Создаём массив с конфигурацией
 
 try:
-	execfile("config.py", config)# Загружаем туда конфигурацию из файла
+	exec(compile(open("config.py", "rb").read(), "config.py", 'exec'), config)# Загружаем туда конфигурацию из файла
 except IOError:
-	print "No configuration file found, to create it, run auth.py"
+	print("No configuration file found, to create it, run auth.py")
 	quit(1)
 	
 url = "https://oauth.vk.com/token?grant_type=password&client_id=3697615&client_secret=AlVXZFMUqyrnABp8ncuU&username=%s&password=%s" % (config['username'], config['password'])
 
 try:
-    r = urllib2.urlopen(url)# Переходим по ссылке(логинимся)
-except urllib2.HTTPError:
-    print "Authorization failed"
+    r = urllib.request.urlopen(url)# Переходим по ссылке(логинимся)
+except urllib.error.HTTPError:
+    print("Authorization failed")
     quit(1)
 
 r = r.read()# Читаем, что нам вернул сайт
@@ -31,7 +31,7 @@ zeroone = [0,1]
 useBigSmiles = random.choice(zeroone)
 
 now = datetime.datetime.now()
-print "Current date: %s.%s" % (now.day, now.month)
+print("Current date: %s.%s" % (now.day, now.month))
 
 def sendMessage():
 	try:
@@ -40,22 +40,22 @@ def sendMessage():
 		for i in range(0, fCount):
 			time.sleep(0.5)
 			r = api.friends.get(count = 1, fields = "bdate", offset = i)[0]
-			if u'bdate' in r:
-				r1 = r[u'bdate']
+			if 'bdate' in r:
+				r1 = r['bdate']
 				birthDate = r1.split(".")
-				print "Birth date: %s.%s" % (birthDate[0], birthDate[1])
+				print("Birth date: %s.%s" % (birthDate[0], birthDate[1]))
 				if (int(birthDate[0]) == now.day) and (int(birthDate[1]) == now.month):
 					if(useBigSmiles == 1):
 						r = api.messages.send(peer_id = r['uid'], sticker_id = random.choice(bigSmiles), v = 5.38)
 					else:
 						r = api.messages.send(peer_id = r['uid'], message = random.choice(littleemoji), v = 5.38)
 			else:
-				print "Birthday is not set by user"
+				print("Birthday is not set by user")
 	except KeyboardInterrupt:
 		pass
 	except vk.exceptions.VkAPIError as e:
-		print "==========ERROR=========="
-		print e
-		print "========================="
+		print("==========ERROR==========")
+		print(e)
+		print("=========================")
 
 sendMessage()
